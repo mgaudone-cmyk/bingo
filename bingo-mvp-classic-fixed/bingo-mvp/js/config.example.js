c8dcc7b5cfbbdbd25d76439001e8e138961683bc
mgaudone-cmyk <mgaud.one@gmail.com>
2026-05-25 15:24:22 -0400

    Add files via upload

---
+++ bingo-mvp-classic-fixed/bingo-mvp/js/config.example.js
@@ -0,0 +1,12 @@
+window.BINGO_CONFIG = {
+  demoMode: true,
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
+};
