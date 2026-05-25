8ffacdd8bc37d8e8499a4f703b50ded6776c3854
mgaudone-cmyk <mgaud.one@gmail.com>
2026-05-25 16:17:59 -0400

    Update config.example.js

--- bingo-mvp-classic-fixed/bingo-mvp/js/config.example.js
+++ bingo-mvp-classic-fixed/bingo-mvp/js/config.example.js
@@ -1,12 +1,12 @@
 window.BINGO_CONFIG = {
-  demoMode: true,
+  demoMode: false,
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyAeGSyOtLfSE8aBufA6LN1iQyt6Gm6eDDk",
+    authDomain: "bingo-mvp.firebaseapp.com",
+    databaseURL: "https://bingo-mvp-default-rtdb.firebaseio.com",
+    projectId: "bingo-mvp",
+    storageBucket: "bingo-mvp.firebasestorage.app",
+    messagingSenderId: "1087429412954",
+    appId: "1:1087429412954:web:abfe20a92627fe86b6f063"
   }
 };
