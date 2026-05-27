81dbf16a9df51c24b34ec592739b6279979b1c97
mgaudone-cmyk <mgaud.one@gmail.com>
2026-05-27 13:05:28 -0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,12 @@
+window.BINGO_CONFIG = {
+  demoMode: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAeGSyOtLfSE8aBufA6LN1iQyt6Gm6eDDk",
+    authDomain: "bingo-mvp.firebaseapp.com",
+    databaseURL: "https://bingo-mvp-default-rtdb.firebaseio.com",
+    projectId: "bingo-mvp",
+    storageBucket: "bingo-mvp.firebasestorage.app",
+    messagingSenderId: "1087429412954",
+    appId: "1:1087429412954:web:abfe20a92627fe86b6f063"
+  }
+};
